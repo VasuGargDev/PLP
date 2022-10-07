9e9ea73cacb70a82301ba24017c5119ed2efc4f9
Vasu Garg <96354306+VasuGargDev@users.noreply.github.com>
2022-10-07 17:41:07 +0530

    LGTM

--- filter.js
+++ filter.js
@@ -1,7 +1,6 @@
 
 let grid = document.querySelector(".products");
 
-
 //fetching data from API
 fetch('./database/store.json')
     .then(res => res.json()) 
@@ -12,7 +11,6 @@ fetch('./database/store.json')
         }
         
     });
-
 
 // Getting values from API and cteating dynamic element
 function addElement(appendIn, value){
@@ -33,21 +31,15 @@ function addElement(appendIn, value){
     appendIn.appendChild(div);
 }
 
-
-
-
 let filterInput = document.getElementById("filterInput");
 
 //adding event listener
 filterInput.addEventListener('keyup', filterProducts);
 
-
-
 function filterProducts(){
     let filterValue = filterInput.value.toUpperCase();
     let item = grid.querySelectorAll('.item')
     
-
     for (let i = 0; i < item.length; i++){
         let span = item[i].querySelector('.title');
 
@@ -56,11 +48,8 @@ function filterProducts(){
         }else{
             item[i].style.display = "none";
         }
-
     }
 }
-
-
 
 //filtering function for search bar
 function filterProducts(){
@@ -78,9 +67,7 @@ function filterProducts(){
         for (const value of filterData){
             addElement(grid, value)
         }
-        
     });
-
 }
 
 //match function for filtering by text search based upon regular expression
@@ -89,8 +76,6 @@ const match = (values, filterby, input) => {
     const re = RegExp(p);
     return values.filter(v => v[filterby].toUpperCase().match(re))
 }   
-
-
 
 //filtering function for categories
 function filterBtnProducts(itemCategory){
@@ -112,5 +97,4 @@ function filterBtnProducts(itemCategory){
         }
 
     });
-
 }
